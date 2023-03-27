5b734ba4cdb49fdef0d069181d53c762bac9e93e
Parabeac-Cloud <admin@parabeac.com>
2023-03-27 23:14:31 +0000

    Auto-generated commit by Parabeac Cloud

--- README.md
+++ README.md
@@ -1,24 +1,16 @@
-# Stateless Hello World
+# zorg
 
-A new Flutter project created with [FlutLab](https://flutlab.io)
+A new Flutter project.
 
-### Getting Started: FlutLab - Flutter Online IDE
-
-- Watch Mini crash course "How to use Flutter Widgets” on [FlutLab Youtube Channel](https://www.youtube.com/channel/UC7ZOPQm4JFlvBc9$
-- Discover a marketplace of ready-to-use Flutter projects [FlutLab Widget Bay](https://widgetbay.flutlab.io/)
-- Join the discussion and conversation on [FlutLab Facebook Group](https://www.facebook.com/groups/flutlab/)
-
-If you have some questions regarding FlutLab, you can ask on [FlutLab FAQ](https://faq.flutlab.io/)
-
-### Getting Started: Flutter
+## Getting Started
 
 This project is a starting point for a Flutter application.
 
 A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-For help getting started with Flutter, view
-[online documentation](https://flutter.dev/docs), which offers tutorials,
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
---
+++ lib/main_widgetbook.g.dart
@@ -0,0 +1,69 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+    import 'package:widgetbook/widgetbook.dart';
+    import 'package:flutter/material.dart';
+    import 'package:zorg/widgets/page_1/register_as_button.g.dart';
+
+
+    void main() {
+      runApp(const MyApp());
+    }
+
+    class MyApp extends StatelessWidget {
+      const MyApp({Key? key}) : super(key: key);
+
+      @override
+      Widget build(BuildContext context){
+        return Widgetbook(
+          themes: [
+            WidgetbookTheme(name: 'Light', data: ThemeData.light()),
+          ],
+          devices: const [
+            Apple.iPhone11ProMax,
+            Samsung.s10,
+          ],
+          categories: [
+                  WidgetbookCategory(
+        name: 'Parabeac-Generated',
+        folders: [
+      WidgetbookFolder(
+        name: 'page_1',
+        widgets: [
+      WidgetbookWidget(
+        name: 'Register--as-button',
+        useCases: [
+      WidgetbookUseCase(
+        name: 'RegisterAsButton',
+        builder: (context) => Center(child:       SizedBox(
+        height: 48.0,width: 354.0,
+        child: RegisterAsButton(
+)
+,
+      ),
+    ),
+      ),
+    
+],
+
+      )
+    
+],
+
+      ),
+    
+],
+
+        
+      )
+    ,
+          ],
+          appInfo: AppInfo(name: 'MyApp'),
+        );
+      }
+    }
+    
